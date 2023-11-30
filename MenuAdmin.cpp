957ea96645b9761762290a9e488796fc046f4849
“Darwin <00042223@uca.edu.sv>
2023-11-29 23:48:09 -0600

    Agregado manejo de eventos y funciones de administrador :heavy_plus_sign:

--- MenuAdmin.cpp
+++ MenuAdmin.cpp
@@ -32,3 +32,67 @@ struct SolicitudEvento {
 vector<SolicitudEvento> solicitudesEventos; // Lista de solicitudes de eventos pendientes
 vector<Evento> eventos; // Lista de eventos existentes
 string usuarioActual = "Admin"; // Usuario actual (inicializado como 'Admin')
+
+// Declaraciones de funciones
+bool iniciarSesion(bool esAdmin);
+void registrarUsuario(bool esAdmin);
+void menuAdmin();
+void menuUsuario();
+void crearEvento();
+void mostrarEventos();
+void modificarEvento(Evento& e);
+void buscarEventosPorSimilitud();
+void eliminarEvento(int index);
+void inscribirseEnEvento();
+void verificarMisEventos();
+void enviarSolicitudEvento();
+void verSolicitudesDeEvento();
+
+// Funcion para limpiar la pantalla de la consola 
+void limpiarPantalla() {
+    system("cls");
+}
+
+// Funcion para crear un nuevo evento y agregarlo al vector 'eventos'.
+void crearEvento() {
+    Evento e; // Se crea una instancia de la estructura 'Evento' para almacenar la informacion del nuevo evento.
+    cout << "Ingrese el nombre del evento o '0' para salir: ";
+    cin.ignore(); 
+    getline(cin, e.nombre); // Se lee la línea de entrada (nombre del evento) y se almacena en 'e.nombre'.
+    if (e.nombre == "0") { // Si el nombre ingresado es "0", se cancela la creación del evento.
+        return;
+    }
+    cout << "Ingrese la descripcion del evento o '0' para salir: ";
+    getline(cin, e.descripcion); // Se lee la descripcion del evento y se almacena en 'e.descripcion'.
+    if (e.descripcion == "0") { // Si la descripcion ingresada es "0", se cancela la creación del evento.
+        return;
+    }
+    cout << "Ingrese la fecha del evento o '0' para salir: ";
+    getline(cin, e.fecha); // Se lee la fecha del evento y se almacena en 'e.fecha'.
+    if (e.fecha == "0") { // Si la fecha ingresada es "0", se cancela la creacion del evento.
+        return;
+    }
+    cout << "Ingrese el lugar del evento o '0' para salir: ";
+    getline(cin, e.lugar); // Se lee el lugar del evento y se almacena en 'e.lugar'.
+    if (e.lugar == "0") { // Si el lugar ingresado es "0", se cancela la creación del evento.
+        return;
+    }
+    cout << "Ingrese la hora del evento o '0' para salir: ";
+    getline(cin, e.hora); // Se lee la hora del evento y se almacena en 'e.hora'.
+    if (e.hora == "0") { // Si la hora ingresada es "0", se cancela la creación del evento.
+        return;
+    }
+    cout << "Ingrese la cantidad de personas o '0' para salir: ";
+    if (!(cin >> e.cantidadPersonas)) { // Se verifica si la entrada es un numero valido.
+        cin.clear(); // Se limpia el estado de error de cin.
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Se ignora la entrada incorrecta.
+        return;
+    }
+    if (e.cantidadPersonas == 0) { // Si la cantidad de personas es 0, se cancela la creacion del evento.
+        return;
+    }
+    cin.ignore(); // Se ignora el salto de linea pendiente en el búfer de entrada.
+    eventos.push_back(e); // Se agrega el evento al vector 'eventos'.
+    cout << "Evento creado exitosamente. Presione Enter para continuar...";
+    cin.get(); // Espera a que se presione Enter antes de continuar.
+}

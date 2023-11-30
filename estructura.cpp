caea7fea4b440e53dab7a901f856429831e1c1c7
WilberCalderon <00042623@uca.edu.sv>
2023-11-29 23:17:45 -0600

    Agrega definiciones de contenedores y función para buscar eventos por similitud de nombre

--- estructura.cpp
+++ estructura.cpp
@@ -38,3 +38,41 @@ struct Evento {
     vector<string> inscritos; // Lista de nombres de usuarios inscritos en el evento
     string estado;          // Estado actual del evento (activo, cancelado, etc.)
 };
+// Definición de contenedores para almacenar diferentes tipos de datos
+
+vector<SolicitudEvento> solicitudesEventos; // Vector para almacenar solicitudes de eventos
+vector<Usuario> usuarios;                   // Vector para almacenar usuarios
+vector<Usuario> administradores;            // Vector para almacenar administradores
+vector<Evento> eventos;                     // Vector para almacenar eventos
+string usuarioActual;                       // Almacena el nombre del usuario actual
+
+// Mapa para asociar un nombre de usuario con un vector de nombres de eventos solicitados
+map<string, vector<string>> solicitudesUsuario; 
+
+// Función para obtener eventos por similitud de nombre
+vector<Evento*> ObtenerEventosPorSimilitud() {
+    vector<Evento*> eventosEncontrados; // Vector para almacenar punteros a eventos encontrados
+
+    cout << "Ingrese parte del nombre del evento a buscar: ";
+    string nombreBusqueda;
+    cin.ignore(); // Ignora el buffer de entrada para limpiar cualquier entrada previa
+    getline(cin, nombreBusqueda); // Obtiene la línea de entrada y la almacena en nombreBusqueda
+
+    // Recorre todos los eventos y busca coincidencias en el nombre
+    for (Evento& e : eventos) {
+        // Si el nombre del evento contiene la cadena de búsqueda
+        if (e.nombre.find(nombreBusqueda) != string::npos) {
+            eventosEncontrados.push_back(&e); // Añade el puntero al evento al vector de eventos encontrados
+        }
+    }
+
+    return eventosEncontrados; // Devuelve el vector de punteros a eventos encontrados
+}
+// Función para limpiar la pantalla de la consola
+void limpiarPantalla() {
+    #ifdef _WIN32
+        system("cls"); // Si el sistema operativo es Windows, usa el comando 'cls'
+    #else
+        system("clear"); // En otros sistemas operativos, usa el comando 'clear'
+    #endif
+}
